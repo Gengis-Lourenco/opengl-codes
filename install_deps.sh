eef4e24fd41bfa54f4bfe93034a35f2d13790476
Gengis <gglourenco2020@gmail.com>
2025-04-30 15:20:19 +0200

    Fix unbound PKG_CONFIG_PATH on macOS

--- install_deps.sh
+++ install_deps.sh
@@ -20,7 +20,7 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
   brew install cmake pkg-config glfw libomp
 
   # macOS: s’assurer que pkg-config voit les .pc de Homebrew
-  export PKG_CONFIG_PATH="$(brew --prefix)/lib/pkgconfig:$PKG_CONFIG_PATH"
+  export PKG_CONFIG_PATH="$(brew --prefix)/lib/pkgconfig:{$PKG_CONFIG_PATH:-}"
 
 elif [[ "$OSTYPE" == "msys"* || "$OSTYPE" == "cygwin"* || "$OS" == "Windows_NT" ]]; then
   echo "→ Windows detected: installing via vcpkg"
